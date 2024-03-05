3c6bcbb181a5e292d9728ad220d08755b2f218c2
Mousom <sarkarmousom8@gmail.com>
2024-03-05 20:03:28 +0530

    Added search Functionality

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -15,6 +15,29 @@ const Search = ({
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = '';
+
+      if(query) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'query',
+          value: query
+        })
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ['query']
+        })
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300)
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query, searchParams, router])
+
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image
